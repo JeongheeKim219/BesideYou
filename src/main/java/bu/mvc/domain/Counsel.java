2925ffc3c49ad2e4eee750995a2bc7ae87e67086
kkyun0823 <kkyun0823@naver.com>
2021-06-10 15:19:06 +0900

    Update Counsel.java
    counsel domain 수정

--- src/main/java/bu/mvc/domain/Counsel.java
+++ src/main/java/bu/mvc/domain/Counsel.java
@@ -3,7 +3,10 @@ package bu.mvc.domain;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,9 +21,15 @@ import lombok.Setter;
 public class Counsel {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "counsel_seq")
+	@SequenceGenerator(sequenceName = "counsel_seq",allocationSize = 1, name = "counsel_seq")
 	private Long counselCode;
+	
+	
 	private Long memberCode;
+	
 	private Long counselorCode;
+	
 	private int counselCategory;
 	private int counselState;
 	private LocalDateTime counselReqDate;
