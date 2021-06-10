321c55b5204733ecf87417f749afa8220a3cad15
Jeonghee <79793074+JeongheeKim219@users.noreply.github.com>
2021-06-10 14:06:51 +0900

    Revert "Merge branch 'Jeonghee-admin' into MinSub"
    This reverts commit 5a1678e68714656cd332c10ca522d4779c4ee00a, reversing
    changes made to 19594dc17beccb0445d2d8f3579e9e61993003b3.

--- src/main/java/bu/mvc/domain/Member.java
+++ src/main/java/bu/mvc/domain/Member.java
@@ -1,8 +1,9 @@
 package bu.mvc.domain;
 
+
+
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,14 +11,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-@NoArgsConstructor
-@Getter
-@Setter
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 	
 	@Id
@@ -36,21 +38,7 @@ public class Member {
 	
 	private LocalDate dateOfBirth;//생년월일
 	
+	
 	private Date dateOfRegDate;//가입일
-		public Member(Long memberCode, String id, String password, String name, String alias, String addr, String phone,
-			String email, int state, LocalDate dateOfBirth, Date dateOfSign) {
-		this.memberCode = memberCode;
-		this.id = id;
-		this.password = password;
-		this.name = name;
-		this.alias = alias;
-		this.addr = addr;
-		this.phone = phone;
-		this.email = email;
-		this.state = state;
-		this.dateOfBirth = dateOfBirth;
-		this.dateOfSign = dateOfSign;
-	}
-
-
+	
 }
--- src/test/java/bu/mvc/BesideYouApplicationTests.java
+++ src/test/java/bu/mvc/BesideYouApplicationTests.java
@@ -1,10 +1,5 @@
 package bu.mvc;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
@@ -12,31 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 
-import bu.mvc.domain.Member;
 import bu.mvc.domain.TestJPA;
-import bu.mvc.respsitory.AdminRepository;
 import bu.mvc.respsitory.TestRepository;
 
 @SpringBootTest
 @Transactional
 @Commit
 class BesideYouApplicationTests {
-	
+
 	@Autowired
-	private AdminRepository adminRep;
+	private TestRepository testRep;
 
-
-	
 	@Test
-	void newMember() {
-		LocalDateTime start = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(0,0,0));
-		LocalDateTime end = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,59));
-		
-		List<Member> newMemeberList =  adminRep.findByDateOfRegBetween(start, end);
-		newMemeberList.forEach(m -> System.out.println(m));
-	
+	void insert() {
+		testRep.save(new TestJPA(1, "확인1"));
 	}
-	
-	
 
 }
