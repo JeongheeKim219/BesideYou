a2240e54bd781bea586ae940b0249a249eb9f076
kkyun0823 <kkyun0823@naver.com>
2021-06-10 23:18:03 +0900

    junittest용
    3개 테이블 완료

--- src/main/java/bu/mvc/domain/RequestAns.java
+++ src/main/java/bu/mvc/domain/RequestAns.java
@@ -6,7 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,17 +25,28 @@ import lombok.Setter;
 @AllArgsConstructor
 public class RequestAns {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "request_seq")
-	@SequenceGenerator(sequenceName = "request_seq",allocationSize = 1, name = "request_seq")	
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "request_ans_seq")
+	@SequenceGenerator(sequenceName = "request_ans_seq",allocationSize = 1, name = "request_ans_seq")	
 	private Long ansCode;
 	
-	private Long requestCode;
+	@OneToOne
+	@JoinColumn(name = "request_code")
+	private Requests requests;
 	
-	private Long counselorCode;
+	@ManyToOne
+	@JoinColumn(name = "COUNSELOR_CODE")
+	private Counselor counselor;
 	
 	private String ansContent;
 	
+	@CreationTimestamp
 	private Date ansDate;
-	
+
+	public RequestAns(Long ansCode, Requests requests, Counselor counselor, String ansContent) {
+		this.ansCode = ansCode;
+		this.requests = requests;
+		this.counselor = counselor;
+		this.ansContent = ansContent;
+	}
 	
 }
--- src/main/java/bu/mvc/domain/Requests.java
+++ src/main/java/bu/mvc/domain/Requests.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 import lombok.AllArgsConstructor;
@@ -16,17 +18,24 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Request {
+public class Requests {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "request_seq")
 	@SequenceGenerator(sequenceName = "request_seq",allocationSize = 1, name = "request_seq")
 	private Long requestCode;
 	
-	private Long counselCode;
+	@OneToOne
+	@JoinColumn(name = "COUNSEL_CODE")
+	private Counsel counsel;
 	
 	private String requestTitle;
 	
 	private int requestCategory;
 	
 	private String requestContent;
+
+	public Requests(Long requestCode) {
+		this.requestCode = requestCode;
+	}
+	
 }
