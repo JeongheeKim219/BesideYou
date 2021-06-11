836819d8e8bc8d008443af7f701a88d12bb7e596
Nicodemus421 <79410466+Nicodemus421@users.noreply.github.com>
2021-06-11 17:53:34 +0900

    ticket service settings 210611006
    ticket service settings 210611006

--- src/main/java/bu/mvc/service/RefundService.java
+++ src/main/java/bu/mvc/service/RefundService.java
@@ -1,27 +1,39 @@
 package bu.mvc.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import bu.mvc.domain.Refund;
+
 public interface RefundService {
 
 	/**
 	 * 환불 신청 목록 전체 보기
 	 * */
+	Page<Refund> selectAll(Pageable pageable);
 	
 	/**
-	 * 환불 신청 내역 검색하기
-	 *  - 회원 id, 상담권 종류, 신청 날짜 등에 따라서
+	 * 회원 id로 환불 신청 내역 검색하기
+	 *  - 회원 id로 검색하여 사용자 마이페이지에서 출력
 	 * */
+	List<Refund> searchBy(String id, Pageable pageable);
 	
 	/**
 	 * 환불 신청 내역 상세보기
 	 * */
+	Refund selectBy(Long refundCode);
 	
 	/**
 	 * 환불 신청 내역 수정하기
 	 *  - 환불 완료 후 상태 변경 등
 	 * */
 	
+	
 	/**
 	 * 환불 신청 내역 삭제하기
 	 * */
+	
 	
 }
