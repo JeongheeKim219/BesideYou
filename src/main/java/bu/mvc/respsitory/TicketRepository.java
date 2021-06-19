1e11ca0a98e229832de6eaa3cd162d064c683106
kkyun0823 <kkyun0823@naver.com>
2021-06-20 01:10:15 +0900

    ticket rep
    commit

--- src/main/java/bu/mvc/respsitory/TicketRepository.java
+++ src/main/java/bu/mvc/respsitory/TicketRepository.java
@@ -1,27 +1,22 @@
 package bu.mvc.respsitory;
 
-
 import java.util.List;
-
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import bu.mvc.domain.Ticket;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long>{
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
 	@Query("select t from Ticket t where t.member.memberCode=?1 and t.counselor.counselorCode=?2 and t.ticketField=?3 and t.ticketRemain!=0 order by ticketDate")
 	List<Ticket> getRemainTicket(Long memberCode, Long counselorCode, int ticketField);
 
-	
 	Page<Ticket> findByMemberId(String id, Pageable pageable);
 
 	/**
@@ -29,5 +24,11 @@ public interface TicketRepository extends JpaRepository<Ticket, Long>{
 	 */
 	List<Ticket> findByTicketDateBetween(LocalDateTime start, LocalDateTime end);
 
-	
+	/**
+	 * 2. 전체 상품권 매출 조회
+	 */
+	Page<Ticket> findAll(Pageable pageable);
+
+	List<Ticket> findByMemberMemberCode(Long memberCode);
+
 }
