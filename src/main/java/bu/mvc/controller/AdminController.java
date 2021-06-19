9c3cd59506a064a2df0c596cc640f0df7eda94f4
gnstjrdlsla <67942521+gnstjrdlsla@users.noreply.github.com>
2021-06-19 17:33:22 +0900

    commit

--- src/main/java/bu/mvc/controller/AdminController.java
+++ src/main/java/bu/mvc/controller/AdminController.java
@@ -29,9 +29,11 @@ import bu.mvc.domain.ContactReply;
 import bu.mvc.domain.Counsel;
 import bu.mvc.domain.Counselor;
 import bu.mvc.domain.Member;
+import bu.mvc.domain.Ticket;
 import bu.mvc.respsitory.CounselorRepository;
 import bu.mvc.respsitory.MemberRepository;
 import bu.mvc.service.AdminService;
+import bu.mvc.service.TicketService;
 
 @Controller
 @RequestMapping("/admin")
@@ -45,6 +47,9 @@ public class AdminController {
 	
 	@Autowired
 	private CounselorRepository counselorRep;
+	
+	@Autowired
+	private TicketService ticketService;
 	
 	
 	/**
@@ -156,31 +161,7 @@ public class AdminController {
 	}
 
 	
-	/**
-	 * 12. 문의내역 처리 게시판으로 이동 +) 문의내역 검색
-	 */
-	@RequestMapping("/contactView")
-	public String viewContact(Model model, @RequestParam(defaultValue = "0") int currentPage) {
 
-		Pageable pageable = PageRequest.of(currentPage, 10, Direction.DESC, "contactCode");
-		Page<Contact> contactPageList = adminService.selectAllContact(pageable);
-		model.addAttribute("contactPageList", contactPageList);
-		return "admin/contactView";
-	}
-
-	
-	/**
-	 * 13. 문의 상세보기 페이지로 이동
-	 * 
-	 * @param contactCode
-	 * @return "admin/contactDetailView"
-	 */
-	@RequestMapping("/viewContactDetail/{contactCode}")
-	public String viewContactDetail(Model model, @PathVariable Long contactCode) {
-		Contact contact = adminService.selectContactById(contactCode);
-		model.addAttribute("contact", contact);
-		return "admin/contactDetailView";
-	}
 
 	
 	/**
@@ -295,6 +276,20 @@ public class AdminController {
 		  
 		  return "admin/memberView";
 	  }
+	  
+	  
+	@RequestMapping("/ticketView")
+	public ModelAndView list(@RequestParam(defaultValue = "0") int nowPage) {
+		Pageable pageable = PageRequest.of(nowPage, 6, Direction.DESC, "ticketCode");
+		Page<Ticket> tkList = ticketService.selectAll(pageable);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("admin/ticketView");
+		mv.addObject("tkList", tkList);
+		mv.addObject("previous", pageable.previousOrFirst().getPageNumber());
+		mv.addObject("next", pageable.next().getPageNumber());
+		return mv;
+	}
 	
 }
 
