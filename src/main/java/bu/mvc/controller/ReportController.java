a670349af83c7bf8f35fc63932950451723fed21
simgihun <78849245+simgihun@users.noreply.github.com>
2021-06-20 18:20:27 +0900

    commit

--- src/main/java/bu/mvc/controller/ReportController.java
+++ src/main/java/bu/mvc/controller/ReportController.java
@@ -1,18 +1,13 @@
 package bu.mvc.controller;
 
-import javax.print.attribute.standard.Media;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
