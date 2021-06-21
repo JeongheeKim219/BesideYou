21629484af557fe47485b2b24239933692bb3855
simgihun <78849245+simgihun@users.noreply.github.com>
2021-06-22 03:34:26 +0900

    Revert "Revert "Revert "리뷰 리스트 연결 완료.."""
    This reverts commit 19edbd50c120cef9a10a247cc3c83c0970288c86.

--- src/main/java/bu/mvc/controller/ReviewController.java
+++ src/main/java/bu/mvc/controller/ReviewController.java
@@ -26,7 +26,14 @@ public class ReviewController {
 	@Autowired
 	private ReviewService reviewService;
 	
-
+	/**
+	 * 리뷰폼
+	 * */
+//	@RequestMapping("/review/reviewForm")
+//	public void reviewForm() {
+//		
+//	}
+	
 	/**
 	 * 리뷰 등록
 	 * */
@@ -42,9 +49,54 @@ public class ReviewController {
 		return "redirect:/counsel/myCounselList?field=-1";
 	}
 	
-
-
-
+	
+	
+	
+	/**
+	 * 모든 리뷰출력(삭제예정)
+	 * */
+	@RequestMapping("/review/reviewForm")
+	public void selectAll(Model model, @RequestParam(defaultValue = "0") int nowPage) {
+		Pageable pageable = PageRequest.of(nowPage, 100, Direction.DESC, "reviewCode");
+		Page<ReviewStar> pageList = reviewService.selectAll(pageable);
+		System.out.println("aaaa  : "+pageList.toString());
+		model.addAttribute("pageList", pageList);
+	}
+	
+	/**
+	 * 리뷰 입력 테스트용 리스트 출력
+	 * */
+	
+	@RequestMapping("/review/reviewForm/{counselorCode}")
+	public ModelAndView selectByCounselor(@PathVariable Long counselorCode) {
+		List<ReviewStar> revList = reviewService.selectByCounselorCode(counselorCode, null);
+		Double point = reviewService.avgStar(counselorCode);
+		System.out.println(point);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("revList", revList);
+		mv.addObject("avgStar", point);
+		mv.setViewName("review/reviewForm");
+		return mv;
+	}
+	
+	/**
+	 * 상담사 코드에따른 리뷰 출력
+	 * */
+	
+		@RequestMapping("/review/reviewByCode")
+		public ModelAndView selectByCounselor(@PathVariable Long counselorCode, @PageableDefault(size = 5, sort = "reviewCode", direction = Sort.Direction.DESC) Pageable pageable) {
+			System.out.println("aa");
+			List<ReviewStar> revList = reviewService.selectByCounselorCode(counselorCode, pageable);
+			Double point = reviewService.avgStar(counselorCode);
+			System.out.println(point);
+			ModelAndView mv = new ModelAndView();
+			mv.addObject("revList", revList);
+			mv.addObject("avgStar", point);
+			mv.addObject("previous", pageable.previousOrFirst().getClass());
+			mv.addObject("next", pageable.next().getPageNumber());
+			mv.setViewName("review/reviewByCode");
+			return mv;
+		}
 	
 	
 	/**
@@ -52,7 +104,6 @@ public class ReviewController {
 	 * */
 		@RequestMapping("/review/reviewUpdate")
 		public String updateReview(ReviewStar review , String reviewContent22) {//reviewContent
-			
 			String content = review.getReviewContent();
 			
 			review.setReviewContent(reviewContent22);
@@ -64,9 +115,14 @@ public class ReviewController {
 			Long cscode = cs.getCounselorCode();
 			
 			reviewService.update(review);
-			return "redirect:/counsel/profile?counselorCode="+cscode;
+			return "redirect:/review/reviewByCode/"+cscode;
 		}
+		
+		
+		
 
+	
+	
 	/**
 	 * 리뷰 삭제
 	 * */
@@ -80,10 +136,13 @@ public class ReviewController {
 		System.out.println(cscode);
 		reviewService.delete(rc);
 		
-		return "redirect:/counsel/profile?counselorCode="+cscode;
+		
+		
+		return "redirect:/review/reviewByCode/"+cscode;
 	}
 	
-
+	
+	
 	
 
 }
